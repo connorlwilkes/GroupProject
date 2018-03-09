1f1b79b1c7a834a504efc79c13af1fa75839fb01
OnwardPyrite <37115532+OnwardPyrite@users.noreply.github.com>
2018-03-09 13:24:01 +0000

    Add files via upload
    Java code to connect to the database and produce an array of 5, random questions.

--- src/Server/QuestionClass.java
+++ src/Server/QuestionClass.java
@@ -1,35 +1,72 @@
-package Server;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+
 
 public class QuestionClass {
 
-    /*
-     * get question header will return the title of the question to be displayed by the GUI.
-     */
-    public static String[] getQuestionHeader() throws ClassNotFoundException {
-        ;
-        int[] result = new int[3];
-        HashSet<Integer> used = new HashSet<Integer>();
-        for (int i = 0; i < 3; i++) {
-            int add = (int) (Math.random() * 5);
-            while (used.contains(add)) {
-                add = (int) (Math.random() * 5);
-            }
-            used.add(add);
-            result[i] = add;
-        }
-        String[] questions = new String[3];
-        for (int i = 0; i < 3; i++) {
-            int questionId = result[i];
-            questions[i] = localQuestionBase.questionBase().get(questionId);
-        }
-        return questions;
-    }
-
+	//generates 5 ids from 1-20, then grabs the question at these ids in the question_table,
+	//then adds them to an array which is finally printed out
     public static void main(String[] args) throws ClassNotFoundException {
+    	
+    	 int[] result = ThreadLocalRandom.current().ints(1, 20).distinct().limit(5).toArray();
+         String[] questions = new String[5];
+		try {
 
-        System.out.println(Arrays.toString(getQuestionHeader()));
+			Class.forName("org.postgresql.Driver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your PostgreSQL JDBC Driver? "
+					+ "Include in your library path!");
+			e.printStackTrace();
+			return;
+
+		}
+		Connection connection = null;
+
+		try {
+
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://mod-msc-sw1.cs.bham.ac.uk:5432/florence", "florence", "kx7t40vm7v");	
+			
+			  Statement stmt = connection.createStatement();
+	          ResultSet rs;
+	          
+	          for (int i = 0; i < 5; i++) {
+
+	             int questionId = result[i];
+	              
+	              stmt = connection.createStatement();
+	              String query = "SELECT question FROM questions_table WHERE id = " + questionId;
+	              rs = stmt.executeQuery(query);
+	               
+	              while (rs.next()) {
+	              String s = rs.getString("question");
+	              questions[i] = s;
+	              }
+	          }
+	          connection.close();
+	            
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		}
+
+		System.out.println(Arrays.toString(questions));
     }
+
 }

f958161c0bbff89a6a95732ccc9c6de2ed3247ee
Connor Wilkes <connorlwilkes@gmail.com>
2018-03-09 20:17:17 +0000

    updated gui with Jack's code

--- src/Client/LogInFrame.java
+++ src/Client/LogInFrame.java
@@ -1,118 +1,70 @@
 package Client;
 
+import Client.ClientGui;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
-public class LogInFrame extends JFrame {
 
-    private JFrame preFrame;
-    private JTextField enterUsername;
-    private JTextField enterPassword;
-    private String username;
-    private String password;
+public class LogInFrame extends JPanel {
 
-    /**
-     * logInDisplay is the username and password entry display, the first dispay that appears when the programme runs.
-     * <p>
-     * JFrame_______________________________
-     * | ___________________________________|       preLeft         preRight
-     * | |								   ||
-     * | |								   ||
-     * | |								   ||
-     * | |		  JPanel = prePanel1	   || ===>  Enter username:  _____________________________________________________
-     * | |								   ||       Enter password:  _____________________________________________________
-     * | |								   ||
-     * | |								   ||
-     * | |                                 ||
-     * | |_________________________________||
-     * | |								   ||
-     * | |								   ||                                                ____________  ________________
-     * | |		  JPanel = prePanel2	   || ===>  Don't have an account? Click 'Register'  | Register |  | Find a lobby |
-     * | |								   ||                                                |__________|  |______________|
-     * | |								   ||
-     * | |								   ||
-     * | ___________________________________|
-     * ______________________________________
-     */
-    public LogInFrame() {
-        this.setBounds(0, 0, 500, 300);
+	    private JTextField enterUsername;
+	    private JTextField enterPassword;
+	    // private String username;
+	    // private String password;
 
-        JLabel enterUsernameLabel = new JLabel("Enter username:");    // the 'Enter Username' label
-        JLabel enterPasswordLabel = new JLabel("Enter password:");    //the 'Enter password' label
-        enterUsername = new JTextField();                            // text box to enter username
-        enterPassword = new JTextField();                            // text box to enter password
-
-        JLabel dontHaveAccount = new JLabel("Don't have an account? Click 'Register'");
-        JButton enterLobby = new JButton("Find a lobby");                //'Find a lobby' button - takes user to the lobby
-        JButton registerNewUser = new JButton("Register");            // 'Register' button - takes user to a registration page
-        JPanel prePanel1 = new JPanel(new GridBagLayout());            // creates a panel and uses GridBagLayout, contains preLeft and preRight
-        JPanel prePanel2 = new JPanel(new GridBagLayout());
-
-        GridBagConstraints preRight = new GridBagConstraints();        // creates the preRight of prePanel1 used for the text fields
-        preRight.anchor = GridBagConstraints.EAST;                    // anchors the preRight position to the right (used on enterUsername and enterPassword panels)
-        preRight.weightx = 2;                                        // weighting for preRight
-        preRight.fill = GridBagConstraints.HORIZONTAL;                // makes the text fields span to fit the width of the frame
-        preRight.gridwidth = GridBagConstraints.REMAINDER;
-
-        GridBagConstraints preLeft = new GridBagConstraints();        // creates the preLeft of prePanel1 used for the labels
-        preLeft.anchor = GridBagConstraints.WEST;                        // anchors the preLeft position to the left (used on enterUsernameLabel and enterPasswordLabel)
-
-        prePanel1.add(enterUsernameLabel, preLeft);                    // adds the enterUsernameLabel to prePanel1
-        prePanel1.add(enterUsername, preRight);                        // adds the text field for username to prePanel1
-        prePanel1.add(enterPasswordLabel, preLeft);                    // adds the enterPasswordLabel to prePanel1
-        prePanel1.add(enterPassword, preRight);                        // adds the text field for password to prePanel1
-
-        prePanel2.add(dontHaveAccount);                                // adds the dontHaveAccountLabel to prePanel2
-        prePanel2.add(registerNewUser);                                // adds the 'Register' button to prePanel2
-        prePanel2.add(enterLobby);                                    // adds the 'Find a lobby' button to prePanel2
-
-        this.add(BorderLayout.CENTER, prePanel1);                    // positions prePanel1 in the center of the frame
-        this.add(BorderLayout.SOUTH, prePanel2);                    // positions prePanel2 at the bottom of the frame
-
-        this.setVisible(true);                                    // makes logInDisplay() visible
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        // closes the JFrame when the display is exitted
-        this.setSize(500, 300);                                    // sets the size of the frame to 500px x 300px (x, y)
-
-        enterLobby.addActionListener(new enterLobbyButtonListener());    // adds action listener on the enterLobby button
-        registerNewUser.addActionListener(new registerNewUserButtonListener());    // adds action listener on the register button
-    }
-
-    /**
-     * 'Enter Lobby' button saves the users username as a variable and takes the user to the lobby page
-     */
-    private class enterLobbyButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            username = enterUsername.getText();
-            password = enterPassword.getText();
-            ClientGui.gui.client.connect();
-            if (username.length() < 1 || password.length() < 1) {
-                JOptionPane.showMessageDialog(ClientGui.gui, "Don't mess with me!", "Warning",
-                        JOptionPane.WARNING_MESSAGE);
-                return;
-            }
-            try {
-                ClientGui.gui.client.logIn(username, password);
-//                    ClientGui.gui.setBounds(ClientGui.gui.getX(), ClientGui.gui.getY(), 300, 200);
-//                    ClientGui.gui.setContentPane(ClientGui.gui.lobby);
-//                    ClientGui.gui.setTitle("Lobby");
-            } catch (IOException e) {
-                e.printStackTrace();
-                ClientGui.gui.client.stop();
-                ClientGui.gui.setVisible(false);
-            }
-        }
-    }
-
-    /**
-     * 'Register' button that takes users from the log-in page to the registration page
-     */
-    class registerNewUserButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            preFrame.setVisible(false);
-            new RegisterFrame();
-        }
-    }
+	    public LogInFrame() {
+	    		setLayout(null);
+	    		this.setBounds(0, 0, 450, 278);
+	    		
+	    		JLabel lblUser = new JLabel("Username: ");
+	    		lblUser.setBounds(110, 113, 79, 16);
+	    		add(lblUser);
+	    		
+	    		enterUsername = new JTextField();
+	    		enterUsername.setBounds(190, 108, 130, 26);
+	    		enterUsername.setColumns(12);
+	    		add(enterUsername);
+	    		
+	    		JLabel lblPass = new JLabel("Password: ");
+	    		lblPass.setBounds(110, 154, 79, 16);
+	    		add(lblPass);
+	    		
+	    		enterPassword = new JTextField();
+	    		enterPassword.setBounds(190, 149, 130, 26);
+	    		enterPassword.setColumns(12);
+	    		add(enterPassword);
+	    		
+	    		JButton btnSignIn = new JButton("Sign In");
+	    		btnSignIn.addActionListener(new ActionListener() {
+	    			public void actionPerformed(ActionEvent e) {
+	    				//ClientGui.gui.setContentPane(
+	    				//		ClientGui.gui.login);
+	    				ClientGui.gui.setTitle("Sign in");
+	    			}
+	    		});
+	    		btnSignIn.setBounds(175, 190, 91, 29);
+	    		add(btnSignIn);
+	    		
+	    		JLabel lblNoAccount = new JLabel("Don't have an account?");
+	    		lblNoAccount.setBounds(150, 250, 180, 16);
+	    		add(lblNoAccount);
+	    			
+	    		JButton btnSignUp = new JButton("Sign Up");
+	    		btnSignUp.addActionListener(new ActionListener() {
+	    			public void actionPerformed(ActionEvent e) {
+	    				// ClientGui.gui.setContentPane(
+	    				//		ClientGui.gui.guiSignUp);
+	    				ClientGui.gui.setTitle("Sign up");
+	    			}
+	    		});
+	    		btnSignUp.setBounds(175, 280, 91, 29);
+	    		add(btnSignUp);
+	    		
+	    		
+	    }
+	
 }
--- src/Client/RegisterFrame.java
+++ src/Client/RegisterFrame.java
@@ -1,93 +1,75 @@
 package Client;
 
+import Client.ClientGui;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class RegisterFrame extends JFrame {
+public class RegisterFrame extends JPanel {
+	
+	 	private JTextField enterUser;
+	    private JTextField enterPass;
+	    private JTextField reEnterPass;
 
-    private JFrame registerFrame;
-    private JTextField enterUsername;
-    private JTextField enterPassword;
-    private JTextField reEnterPassword;
-
-    /**
-     * registerDisplay is the registration display, this allows the user to create a new account
-     * JFrame_______________________________
-     * | ___________________________________|       registerLeft         registerRight
-     * | |								   ||
-     * | |								   ||
-     * | |								   ||       Enter username:  _____________________________________________________
-     * | |	   JPanel = registerPanel1	   || ===>  Enter password:  _____________________________________________________
-     * | |								   ||       Re-enter password:  __________________________________________________
-     * | |								   ||
-     * | |								   ||
-     * | |                                 ||
-     * | |_________________________________||
-     * | |								   ||
-     * | |								   ||             ____________
-     * | |     JPanel = registerPanel2	   || ===>        | Register |
-     * | |								   ||             |__________|
-     * | |								   ||
-     * | |								   ||
-     * | ___________________________________|
-     * ______________________________________
-     */
-    public void registerDisplay() {
-
-        registerFrame = new JFrame("REGISTER");                            // creates a new frame
-
-        JLabel enterUsernameLabel = new JLabel("Enter username:");        // the 'Enter Username' label
-        JLabel enterPasswordLabel = new JLabel("Enter password:");        //the 'Enter password' label
-        JLabel reEnterPasswordLabel = new JLabel("Re-enter password:");    //the 'Re-enter password' label
-        enterUsername = new JTextField();                                // text box to enter username
-        enterPassword = new JTextField();                                // text box to enter password
-        reEnterPassword = new JTextField();                                // text box to re-enter password
-
-        JButton register = new JButton("Register");                        // 'Register' button - takes user to a registration page
-        JPanel registerPanel1 = new JPanel(new GridBagLayout());            // creates a panel and uses GridBagLayout, contains preLeft and preRight
-        JPanel registerPanel2 = new JPanel(new GridBagLayout());
-
-        GridBagConstraints registerRight = new GridBagConstraints();        // creates the registerRight of registerPanel1 used for the text fields
-        registerRight.anchor = GridBagConstraints.EAST;                    // anchors the registerRight position to the right (used on enterUsername and enterPassword panels)
-        registerRight.weightx = 2;                                        // weighting for registerRight
-        registerRight.fill = GridBagConstraints.HORIZONTAL;                // makes the text fields span to fit the width of the frame
-        registerRight.gridwidth = GridBagConstraints.REMAINDER;
-
-        GridBagConstraints registerLeft = new GridBagConstraints();        // creates the regsiterLeft of registerPanel1 used for the labels
-        registerLeft.anchor = GridBagConstraints.WEST;                    // anchors the regsiterLeft position to the left (used on enterUsernameLabel and enterPasswordLabel)
-
-        registerPanel1.add(enterUsernameLabel, registerLeft);                // adds the enterUsernameLabel to registerPanel1
-        registerPanel1.add(enterUsername, registerRight);                    // adds the text field for username to registerprePanel1
-        registerPanel1.add(enterPasswordLabel, registerLeft);                // adds the enterPasswordLabel to registerprePanel1
-        registerPanel1.add(enterPassword, registerRight);                    // adds the text field for password to registerprePanel1
-        registerPanel1.add(reEnterPasswordLabel, registerLeft);            // adds the reEnterPassword label to registerprePanel1
-        registerPanel1.add(reEnterPassword, registerRight);
-
-        registerPanel2.add(register);                                    // adds the 'Register' button to registerPanel2
-
-        registerFrame.add(BorderLayout.CENTER, registerPanel1);
-        registerFrame.add(BorderLayout.SOUTH, registerPanel2);
-
-        registerFrame.setVisible(true);                                    // makes logInDisplay() visible
-        registerFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);            // closes the JFrame when the display is exitted
-        registerFrame.setSize(500, 300);                                    // sets the size of the frame to 500px x 300px (x, y)
-
-        register.addActionListener(new registerPageButtonListener());        // adds action listener on the Register button
-    }
-
-    /**
-     * 'Register' button that stores the new account details in the database and then takes
-     * the user back to the log-in page
-     */
-    class registerPageButtonListener implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
-            // check if username in use already
-            // check if 2 passwords match
-            // if above hold then store new account details in the database
-            registerFrame.setVisible(false);
-            new LogInFrame();
-        }
-    }
+	    public RegisterFrame() {
+	    		setLayout(null);
+	    		this.setBounds(0, 0, 450, 278);
+	    		
+	    		JLabel lblUser = new JLabel("Username: ");
+	    		lblUser.setBounds(120, 113, 79, 16);
+	    		add(lblUser);
+	    		
+	    		enterUser = new JTextField();
+	    		enterUser.setBounds(200, 108, 130, 26);
+	    		enterUser.setColumns(12);
+	    		add(enterUser);
+	    		
+	    		JLabel lblPass = new JLabel("Password: ");
+	    		lblPass.setBounds(120, 154, 79, 16);
+	    		add(lblPass);
+	    		
+	    		enterPass = new JTextField();
+	    		enterPass.setBounds(200, 149, 130, 26);
+	    		enterPass.setColumns(12);
+	    		add(enterPass);
+	    		
+	    		JLabel lblReEnterPass = new JLabel("ReEnter Password: ");
+	    		lblReEnterPass.setBounds(80, 195, 130, 16);
+	    		add(lblReEnterPass);
+	    		
+	    		reEnterPass = new JTextField();
+	    		reEnterPass.setBounds(200, 190, 130, 26);
+	    		reEnterPass.setColumns(12);
+	    		add(reEnterPass);
+	    		
+	    		JButton btnSignIn = new JButton("Sign In");
+	    		btnSignIn.addActionListener(new ActionListener() {
+	    			public void actionPerformed(ActionEvent e) {
+	    				//ClientGui.gui.setContentPane(
+	    				//		ClientGui.gui.login);
+	    				ClientGui.gui.setTitle("Sign in");
+	    			}
+	    		});
+	    		btnSignIn.setBounds(215, 240, 91, 29);
+	    		add(btnSignIn);
+	    		
+	    		JButton back = new JButton("Back");
+	    		back.addActionListener(new ActionListener() {
+	    			public void actionPerformed(ActionEvent e) {
+	    				// ClientGui.gui.setContentPane(
+	    				//		ClientGui.gui.guiSignUp);
+	    				ClientGui.gui.setTitle("Back");
+	    			}
+	    		});
+	    		back.setBounds(125, 240, 91, 29);
+	    		add(back);
+	    		
+	    		
+	    }
+	
 }
+
+
+    

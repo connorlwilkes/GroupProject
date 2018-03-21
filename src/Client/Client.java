7ed62f9d8996e8b2164f6f271443e76494e98d25
lewis963 <37115333+lewis963@users.noreply.github.com>
2018-03-21 18:37:05 +0000

    added correct transitions

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -82,95 +82,133 @@ public class Client implements Runnable {
         while (true) {
             try {
                 Object o = inputStream.readObject();
-                if (o instanceof ServerProtocol) {
-                    ServerProtocol message = (ServerProtocol) o;
-                    System.out.println(message);
-                    if (message.type.startsWith("start")) {
-                        gui.remove(gui.wait);
-                        gui.setContentPane(gui.instructions);
-                        gui.revalidate();
+            if (o instanceof ServerProtocol) {
+            	ServerProtocol message = (ServerProtocol)o;
+            	if (message.type.startsWith("start")) {
+            		gui.setContentPane(gui.instructions);
+            		gui.revalidate();
+                    gui.repaint();
+                    gui.setTitle("instructions");
+            		Thread.sleep(5000);
+            		ServerProtocol getQuestionMaster = new ServerProtocol("get-qm");
+            		outputStream.writeObject(getQuestionMaster);
+                    outputStream.flush();
+                    
+            	}
+            	if (message.type.startsWith("get-qm")) {
+            		gui.remove(gui.instructions);
+         
+            		String questionMaster = message.message[0];
+            		gui.qm.lblNewLabel.setText(questionMaster);
+            		gui.setContentPane(gui.qm);
+            		gui.revalidate();
+                    gui.repaint();
+                    gui.setTitle("qm");
+                    
+            		Thread.sleep(5000);
+            		
+            		ServerProtocol getQuestion = new ServerProtocol("question");
+            		outputStream.writeObject(getQuestion);
+                    outputStream.flush();
+            	}
+            	if (message.type.startsWith("quesiton")) {
+            		gui.remove(gui.qm);
+            		if (message.message[0].startsWith("qm")) {
+            			String question = message.message[1];
+            			gui.questionMasterQuestionPanel.txtrQuestion.setText(question);
+            			gui.setContentPane(gui.questionMasterQuestionPanel);
+                		gui.revalidate();
                         gui.repaint();
-                        gui.setTitle("Instructions");
-                        Thread.sleep(5000);
-                        ServerProtocol getQuestionMaster = new ServerProtocol("get-qm");
-                        outputStream.writeObject(getQuestionMaster);
+                        gui.setTitle("questionmasterQuestionPanel");
+                        
+            			Thread.sleep(5000);
+            			
+            			Thread.sleep(500);
+            			ServerProtocol getAnswer = new ServerProtocol("get-answers");
+            			outputStream.writeObject(getAnswer);
                         outputStream.flush();
-                    }
-                    if (message.type.startsWith("get-qm")) {
-                        gui.remove(gui.instructions);
-                        gui.setContentPane(gui.qm);
-                        gui.revalidate();
+            		}
+            		if (message.message[0].startsWith("notqm")) {
+            			String question = message.message[1];
+            			gui.questionPanel.txtrQuestion.setText(question);
+            			gui.setContentPane(gui.questionPanel);
+                		gui.revalidate();
                         gui.repaint();
-                        gui.setTitle("Question master");
-                        String questionMaster = message.message[0];
-                        gui.qm.lblNewLabel.setText(questionMaster);
-                        gui.qm.setVisible(true);
-                        Thread.sleep(5000);
-                        ServerProtocol getQuestion = new ServerProtocol("question");
-                        outputStream.writeObject(getQuestion);
+                        gui.setTitle("questionPanel");
+            	
+            			Thread.sleep(5000);
+            			
+            			ServerProtocol sendAnswer = new ServerProtocol("answer", gui.questionPanel.playerAnswer);
+            			outputStream.writeObject(sendAnswer);
                         outputStream.flush();
-                        gui.qm.setVisible(false);
-                    }
-                    if (message.type.startsWith("question")) {
-                        if (message.message[0].startsWith("qm")) {
-                            String question = message.message[1];
-                            gui.questionMasterQuestionPanel.txtrQuestion.setText(question);
-                            gui.questionMasterQuestionPanel.setVisible(true);
-                            Thread.sleep(5000);
-                            gui.questionMasterQuestionPanel.setVisible(false);
-                        }
-                        if (message.message[0].startsWith("notqm")) {
-                            String question = message.message[1];
-                            gui.questionPanel.txtrQuestion.setText(question);
-                            gui.questionPanel.txtrQuestion.setVisible(true);
-                            Thread.sleep(5000);
-                            ServerProtocol sendAnswer = new ServerProtocol("answer", gui.questionPanel.playerAnswer);
-                            outputStream.writeObject(sendAnswer);
-                            outputStream.flush();
-                            gui.questionPanel.txtrQuestion.setVisible(false);
-                            Thread.sleep(500);
-                            ServerProtocol getAnswer = new ServerProtocol("get-answer");
-                            outputStream.writeObject(getAnswer);
-                            outputStream.flush();
-                        }
-                    }
-                    if (message.type.startsWith("get-answer")) {
-                        if (message.message[0].startsWith("qm")) {
-                            String player1Answer = message.message[1];
-                            String player2Answer = message.message[2];
-                            gui.questionMasterAnswerPanel.txtrQuestion.setText(gui.questionPanel.txtrQuestion.getText());
-                            gui.questionMasterAnswerPanel.txtrPlayerAnswer.setText(player1Answer);
-                            gui.questionMasterAnswerPanel.txtrPlayerAnswer_1.setText(player2Answer);
-                            gui.questionMasterAnswerPanel.setVisible(true);
-                            while (gui.questionMasterAnswerPanel.voteCast = false) {
-                                Thread.sleep(100);
-                            }
-                            ServerProtocol getScore = new ServerProtocol("getScore");
-                            outputStream.writeObject(getScore);
-                            outputStream.flush();
-                            gui.questionMasterAnswerPanel.setVisible(false);
-                        }
-                        if (message.message[0].startsWith("notqm")) {
-                            String playerAnswer = message.message[1];
-                            String player3Answer = message.message[2];
-                            gui.answerPanel.txtrQuestion.setText(gui.questionPanel.txtrQuestion.getText());
-                            gui.answerPanel.txtrPlayerAnswer.setText(playerAnswer);
-                            gui.answerPanel.txtrPlayerAnswer_1.setText(player3Answer);
-                            gui.answerPanel.setVisible(true);
-                            while (gui.questionMasterAnswerPanel.voteCast = false) {
-                                Thread.sleep(100);
-                            }
-                            gui.answerPanel.setVisible(false);
-                        }
-                    }
-                    if (message.type.startsWith("getScore")) {
-                        String player1Score = message.message[1];
-                        String player2Score = message.message[2];
-                        String player3Score = message.message[3];
-                        gui.scorePanel.textArea.setText(player1Score);
-                        // need an array of the user names attached to the get score call
-                        // also what is the protocol for score
-                    }
+                        
+            			Thread.sleep(500);
+            			ServerProtocol getAnswer = new ServerProtocol("get-answers");
+            			outputStream.writeObject(getAnswer);
+                        outputStream.flush();
+            		}
+            	}
+            	if (message.type.startsWith("get-answer")) {
+            		
+            		if (message.message[0].startsWith("qm")) {
+            			gui.remove(gui.questionMasterQuestionPanel);
+            			
+            			String player1Answer = message.message[1];
+            			String player2Answer = message.message[2];
+            			gui.questionMasterAnswerPanel.txtrQuestion.setText(gui.questionPanel.txtrQuestion.getText());
+            			gui.questionMasterAnswerPanel.txtrPlayerAnswer.setText(player1Answer);
+            			gui.questionMasterAnswerPanel.txtrPlayerAnswer_1.setText(player2Answer);
+            			gui.setContentPane(gui.questionMasterAnswerPanel);
+                		gui.revalidate();
+                        gui.repaint();
+                        gui.setTitle("questionMasteranswerpanel");
+                     
+            			while (gui.questionMasterAnswerPanel.voteCast = false) {
+            				Thread.sleep(100);
+            			}
+            			ServerProtocol getScore = new ServerProtocol("getScore");
+            			outputStream.writeObject(getScore);
+                        outputStream.flush();
+            			
+            		}
+            		if (message.message[0].startsWith("notqm")) {
+            			gui.remove(gui.questionPanel);
+            			String playerAnswer = message.message[1];
+            			String player3Answer = message.message[2];
+            			gui.answerPanel.txtrQuestion.setText(gui.questionPanel.txtrQuestion.getText());
+            			gui.answerPanel.txtrPlayerAnswer.setText(playerAnswer);
+            			gui.answerPanel.txtrPlayerAnswer_1.setText(player3Answer);
+            			
+            			gui.setContentPane(gui.answerPanel);
+                		gui.revalidate();
+                        gui.repaint();
+                        gui.setTitle("answerPanel");
+            			
+            			while (gui.questionMasterAnswerPanel.voteCast = false) {
+            				Thread.sleep(100);
+            			}
+            			ServerProtocol getScore = new ServerProtocol("getScore");
+            			outputStream.writeObject(getScore);
+                        outputStream.flush();
+            		}
+            	}
+            	if (message.type.startsWith("getscores")) {
+            		gui.remove(gui.questionMasterAnswerPanel);
+            		gui.remove(gui.answerPanel);
+            		
+            		String player1Score = message.message[1];
+            		String player2Score = message.message[2];
+            		String player3Score = message.message[3];
+            		gui.scorePanel.textArea.setText(player1Score + "\n" + player2Score + "\n" + player3Score);
+            		
+            		gui.setContentPane(gui.scorePanel);
+            		gui.revalidate();
+                    gui.repaint();
+                    gui.setTitle("scorePanel");
+            	}
+            	
+
+            }
 
                 } else if (o instanceof Message) {
                     Message message = (Message) o;

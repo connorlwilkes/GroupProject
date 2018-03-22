f67e0929607d8776c98564eb5f4265cb3e49a443
Jack <jrw563@student.bham.ac.uk>
2018-03-22 22:49:33 +0000

    gui fixes for lab machines

--- src/Client/QuestionMasterAnswerPanel.java
+++ src/Client/QuestionMasterAnswerPanel.java
@@ -79,7 +79,7 @@ public class QuestionMasterAnswerPanel extends JPanel {
         });
 
         btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
-        btnNewButton.setBounds(480, 145, 125, 50);
+        btnNewButton.setBounds(480, 145, 200, 50);
         add(btnNewButton);
 
         JButton btnNewButton_1 = new JButton("Choose Answer");
@@ -99,7 +99,7 @@ public class QuestionMasterAnswerPanel extends JPanel {
         });
 
         btnNewButton_1.setHorizontalAlignment(SwingConstants.LEFT);
-        btnNewButton_1.setBounds(480, 242, 125, 50);
+        btnNewButton_1.setBounds(480, 242, 200, 50);
         add(btnNewButton_1);
 
     }
